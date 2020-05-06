fd1e856d55937c17ae083fe9bc474035774e38b8
zelbatmi <zakari_a@icloud.com>
2020-05-06 18:23:49 +0000

    V1.0

--- app/src/main/java/digiplus/ma/mydarijaapp/MainActivity.java
+++ app/src/main/java/digiplus/ma/mydarijaapp/MainActivity.java
@@ -1,8 +1,11 @@
 package digiplus.ma.mydarijaapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView Numbers = (TextView) findViewById(R.id.button);
+        TextView Family = (TextView) findViewById(R.id.button2);
+        TextView Colors = (TextView) findViewById(R.id.button3);
+        TextView Phrases = (TextView) findViewById(R.id.button4);
+
+        Numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent NumbersIntent = new Intent(MainActivity.this, NumberActivity.class);
+                startActivity(NumbersIntent);
+            }
+        });
+            Family.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent FamilyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                    startActivity(FamilyIntent);
+            }
+        });
+                Colors.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent ColorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                        startActivity(ColorsIntent);
+                    }
+                });
+                Phrases.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent PhrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                        startActivity(PhrasesIntent);
+                    }
+                });
+
+
     }
 }
--- app/src/main/java/digiplus/ma/mydarijaapp/NumberActivity.java
+++ app/src/main/java/digiplus/ma/mydarijaapp/NumberActivity.java
@@ -1,8 +1,12 @@
 package digiplus.ma.mydarijaapp;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+        LinearLayout NumRootView = (LinearLayout) findViewById(R.id.RootView);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        for ( int Idx = 0; Idx < words.size(); Idx++){
+            TextView N_wordView = new TextView(this);
+            N_wordView.setText(words.get(Idx));
+            NumRootView.addView(N_wordView);
+        }
+
     }
 }
