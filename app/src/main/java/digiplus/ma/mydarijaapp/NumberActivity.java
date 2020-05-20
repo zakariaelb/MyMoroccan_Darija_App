c2dd482657af49b1140024d6b07f93933c62228f
zelbatmi <zakari_a@icloud.com>
2020-05-20 19:28:43 +0000

    V1.0

--- app/src/main/java/digiplus/ma/mydarijaapp/NumberActivity.java
+++ app/src/main/java/digiplus/ma/mydarijaapp/NumberActivity.java
@@ -1,5 +1,7 @@
 package digiplus.ma.mydarijaapp;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,39 @@ import java.util.ArrayList;
 public class NumberActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+
     private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mp) {
@@ -23,20 +58,21 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.words_list);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         //LinearLayout NumRootView = (LinearLayout) findViewById(R.id.RootView);
         final ArrayList<Words> word_s = new ArrayList<Words>();
         //Words w = new Words("One, Ouahed");
         //Word_s.add(w);
-        word_s.add(new Words(R.mipmap.ic_launcher,"One", "Ouahed", R.raw.color_black));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Tow", "Jouj", R.raw.color_red));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Three", "Talata", R.raw.color_white));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Four", "Rabaa", R.raw.color_black));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Five", "Khamssa", R.raw.color_red));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Six", "Sst'a", R.raw.color_white));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Seven", "Sab'a", R.raw.color_black));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Eight", "Tmaniya", R.raw.color_red));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Nine", "Tiss'a", R.raw.color_white));
-        word_s.add(new Words(R.mipmap.ic_launcher,"Ten", "Achara", R.raw.color_black));
+        word_s.add(new Words(R.mipmap.ic_launcher, "One", "Ouahed", R.raw.color_black));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Tow", "Jouj", R.raw.color_red));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Three", "Talata", R.raw.color_white));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Four", "Rabaa", R.raw.color_black));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Five", "Khamssa", R.raw.color_red));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Six", "Sst'a", R.raw.color_white));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Seven", "Sab'a", R.raw.color_black));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Eight", "Tmaniya", R.raw.color_red));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Nine", "Tiss'a", R.raw.color_white));
+        word_s.add(new Words(R.mipmap.ic_launcher, "Ten", "Achara", R.raw.color_black));
         //words.add("One");
         //words.add("Two");
 
@@ -47,17 +83,32 @@ public class NumberActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(NumberActivity.this, "Hello ! ", Toast.LENGTH_LONG).show();
-                Words Words = word_s.get(position);
                 releaseMediaPlayer();
+                Words Words = word_s.get(position);
 
-                //mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.color_red);
-                mMediaPlayer = MediaPlayer.create(NumberActivity.this, Words.getmAudioRES());
-                mMediaPlayer.start();
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                // Request audio focus so in order to play the audio file. The app needs to play a
+                // short audio file, so we will request audio focus with a short amount of time
+                // with AUDIOFOCUS_GAIN_TRANSIENT.
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have audio focus now.
+
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
+                    mMediaPlayer = MediaPlayer.create(NumberActivity.this, Words.getmAudioRES());
+
+                    // Start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
     }
-
     @Override
     protected void onStop() {
         super.onStop();
@@ -65,20 +116,22 @@ public class NumberActivity extends AppCompatActivity {
     }
 
     /**
-         * HELPER METHOD Clean up the media player by releasing its resources.
-         */
+     * HELPER METHOD Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
 
-        private void releaseMediaPlayer(){
-            // If the media player is not null, then it may be currently playing a sound.
-            if (mMediaPlayer != null) {
-                // Regardless of the current state of the media player, release its resources
-                // because we no longer need it.
-                mMediaPlayer.release();
-
-                // Set the media player back to null. For our code, we've decided that
-                // setting the media player to null is an easy way to tell that the media player
-                // is not configured to play an audio file at the moment.
-                mMediaPlayer = null;
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
 
         }
     }
